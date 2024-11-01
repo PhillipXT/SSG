832e3c938585498f8e6cf6985b401a8c5eb164af
PhillipXT <phillip.theriault@spellweaver.com>
2024-11-01 00:58:37 -0600

    Refactored file copy functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os, shutil
+
 from src.textnode import TextNode, TextType
 from src.htmlnode import HTMLNode
 from src.leafnode import LeafNode
@@ -7,6 +9,15 @@ from src.utilities import copy_folder
 from src.markdown_text import extract_markdown_images, extract_markdown_links
 
 def main():
-	copy_folder("./static", "./public")
+
+	dir_path_static = "./static"
+	dir_path_public = "./public"
+
+	print(f"Deleting {dir_path_public}")
+	if os.path.exists(dir_path_public):
+		shutil.rmtree(dir_path_public)
+
+	print("Copying static files to public directory...")
+	copy_folder(dir_path_static, dir_path_public)
 
 main()
--- src/utilities.py
+++ src/utilities.py
@@ -2,40 +2,15 @@ import os, shutil
 
 def copy_folder(source, dest):
 
-	if not os.path.exists(source):
-		raise Exception("Source directory does not exist.")
-	
 	if not os.path.exists(dest):
-		raise Exception("Destination directory does not exist.")
+		print(f"Creating folder {dest}")
+		os.mkdir(dest)
 
-	print(f"Deleting {dest}")
-	shutil.rmtree(dest, ignore_errors=True)
-
-	files = get_file_list(source)
-
-	for file in files:
-		source_path = file[0]
-		source_file = source_path + "/" + file[1]
-		dest_path = source_path.replace(source, dest)
-		print(f"Moving {source_file} to {dest_path}")
-		if not os.path.exists(dest_path):
-			os.mkdir(dest_path)
-		shutil.copy(source_file, dest_path)
-
-def get_file_list(source):
-
-	structure = []
-
-	files = os.listdir(source)
-
-	if files == []:
-		return []
-	
-	for file in files:
-		if os.path.isdir(source + "/" + file):
-			path = source + "/" + file
-			structure.extend(get_file_list(path))
+	for file in os.listdir(source):
+		source_path = os.path.join(source, file)
+		dest_path = os.path.join(dest, file)
+		if os.path.isfile(source_path):
+			print(f"Copying {source_path} to {dest_path}")
+			shutil.copy(source_path, dest_path)
 		else:
-			structure.append((source, file))
-
-	return structure
+			copy_folder(source_path, dest_path)
